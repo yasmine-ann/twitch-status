d6ab70e71488b909bd43a2699ce58cb2e321e86d
Yasmine Triola <yasmine.triola@gmail.com>
2018-05-14 15:39:04 -0400

    Add Stream class and mock data.

---
+++ src/app/Stream.ts
@@ -0,0 +1,7 @@
+export class Stream {
+    displayName: string;
+    game: string;
+    status: string;
+    image: string;
+    url: string;
+}
---
+++ src/app/mock-streams.ts
@@ -0,0 +1,18 @@
+import { Stream } from './Stream';
+
+export const STREAMS: Stream[] = [
+    {
+        displayName: "OverwatchLeague",
+        game: "Overwatch",
+        status: "Playing that one level.",
+        image: "http://via.placeholder.com/350x150",
+        url: "https://www.twitch.tv/overwatchleague"
+    },
+    {
+        displayName: "Warcraft",
+        game: "Warcraft",
+        status: "Playing another level.",
+        image: "http://via.placeholder.com/350x150",
+        url: "https://www.twitch.tv/warcraft"
+    }
+]
