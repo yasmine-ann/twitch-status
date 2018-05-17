a449b366cf32e493c8dbf41574b5f9d7e8f84af6
Yasmine Triola <yasmine.triola@gmail.com>
2018-05-17 13:38:59 -0400

    Add mock data and card structure.

--- src/app/mock-streams.ts
+++ src/app/mock-streams.ts
@@ -5,21 +5,21 @@ export const STREAMS: Stream[] = [
         displayName: "OverwatchLeague",
         game: "Overwatch",
         status: "Playing that one level.",
-        image: "http://via.placeholder.com/350x150",
+        image: "http://via.placeholder.com/320x150",
         url: "https://www.twitch.tv/overwatchleague"
     },
     {
         displayName: "Warcraft",
         game: "Warcraft",
         status: "Playing another level.",
-        image: "http://via.placeholder.com/350x150",
+        image: "http://via.placeholder.com/320x150",
         url: "https://www.twitch.tv/warcraft"
     },
     {
         displayName: "NBA",
         game: "NBA2K",
         status: "Playing NBA2K yes yes.",
-        image: "http://via.placeholder.com/350x150",
+        image: "http://via.placeholder.com/320x150",
         url: "https://www.twitch.tv/nba"
     }
 ];
